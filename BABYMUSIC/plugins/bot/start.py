40939de6a8d22d53fa6b0715e2dfb2c9a6204623
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2026-01-28 08:27:39 +0530

    Update start.py
    Signed-off-by: ✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>

--- BABYMUSIC/plugins/bot/start.py
+++ BABYMUSIC/plugins/bot/start.py
@@ -28,7 +28,7 @@ from strings import get_string
 
 
 YUMI_PICS = [
-"https://files.catbox.moe/xhpqtp.jpg",
+"https://raw.githubusercontent.com/BABY-MUSIC/SPOTIFY_MUSIC/main/BABYMUSIC/assets/assets/public.jpg",
 
 ]
 
